580144b00afed3282b3536305dadc9a8d9b05da2
Guru <Gayah>
2019-08-06 09:52:55 +0200

    socr.sh experimantal

--- bin/socr.sh
+++ bin/socr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 # Dependencies: tesseract-ocr imagemagick gnome-screenshot xclip
 
 SCR="$HOME/Documents/Translate/temp"
@@ -6,22 +6,24 @@ mkdir -p $(dirname $SCR)
 
 gnome-screenshot -a -f $SCR.png
 
-mogrify -modulate 100,0 -resize 400% $SCR.png
+mogrify -modulate 100,0 -resize 400% $SCR.png 
 #should increase detection rate
 
 tesseract $SCR.png $SCR -psm 100 -l jpn
 #cat $SCR.txt | awk '{printf $0}" "' | xclip -selection clipboard
-cat $SCR.txt |
-	tr '\n' ' ' |
-	sed -e 's/  /\n/g' |
-	sed -e 's/- //g' |
-	awk '/[a-z,A-Z][:space:]*$/{
-		printf("\n");
-		print $0;
-		printf("\n")
-	}{
-		print $0
-	}' |
+cat $SCR.txt | 
+	tr '\n' ' ' | 
+	sed -e 's/  /\n/g' | 
+	sed -e 's/- //g' | 
+	awk '
+		/[^.][:space:]*$/{
+			printf("\n");
+			print $0; 
+			printf("\n")
+		}
+		/[.][:space:]*$/{
+			print $0
+		}' |
 	xclip -selection clipboard
 #you can only scan one character at a time
 

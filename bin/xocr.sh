45225434cbb59edbdbba6dd9f295f82a1193a958
Michael Ambrus <michael@gambrus.se>
2021-05-25 11:45:46 +0200

    s* refined to be fully replaceable by x*
    * Pesky trails of gnome dependencies removed
    * x* will if needed run scrot internally which makes i3 config
      simpler
    * Prepared for cumulative xclip if multiple args given

--- bin/xocr.sh
+++ bin/xocr.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-# Dependencies: tesseract-ocr imagemagick
+# Dependencies: scrot tesseract-ocr imagemagick
 
-SCR="$HOME/Documents/Translate/temp"
-mkdir -p $(dirname $SCR)
+function _xocr_one() {
+    echo "$1"
+    local _fn_prefix="$HOME/Documents/Translate/temp"
+    mkdir -p $(dirname $_fn_prefix)
 
-#should increase detection rate
-mogrify -modulate 100,0 -resize 400% "$1"
+    #should increase detection rate
+    mogrify -modulate 100,0 -resize 400% "$1"
 
-tesseract "$1" $SCR -psm 100 -l jpn
+    tesseract "$1" $_fn_prefix -psm 100 -l jpn
 
-#you can only scan one character at a time
-cat $SCR.txt | xclip -selection clipboard
+    #you can only scan one character at a time
+    cat $_fn_prefix.txt | xclip -selection clipboard
+}
+export -f _xocr_one
 
-exit
+
+if [ $# -gt 0 ]; then
+    _xocr_one $1
+else
+    scrot -s -e 'mv $f /tmp/ && _xocr_one /tmp/$f'
+fi
--- bin/xpic.sh
+++ bin/xpic.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# Dependencies: xclip
+# Dependencies: scrot xclip
 
-xclip -selection clipboard -target image/png -i "$1"
+function _xpic_one() {
+    xclip -selection clipboard -target image/png -i "$1"
+}
+export -f _xpic_one
 
-exit $?
+if [ $# -gt 0 ]; then
+    xclip -selection clipboard -target image/png -i "$1"
+else
+    scrot -s -e 'mv $f /tmp/ && _xpic_one /tmp/$f'
+fi
